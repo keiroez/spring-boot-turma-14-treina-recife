48855d78e6a0a2df2be5633334a72b8e2fcb8e64
Izaquiel Queiroz <kyelqueiroz@hotmail.com>
2026-08-15 14:23:52 -0300

    spring aula 3 e 4 -  Camadas e JPA

--- revisao_aula_1_e_2/sgp/src/main/java/com/treinarecife/sgp/services/UsuarioService.java
+++ revisao_aula_1_e_2/sgp/src/main/java/com/treinarecife/sgp/services/UsuarioService.java
@@ -2,17 +2,29 @@ package com.treinarecife.sgp.services;
 
 import com.treinarecife.sgp.models.Usuario;
 import com.treinarecife.sgp.repositories.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UsuarioService {
-    private final UsuarioRepository usuarioRepository;
-
-    public UsuarioService(UsuarioRepository usuarioRepository) {
-        this.usuarioRepository = usuarioRepository;
-    }
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
     public void inserir(Usuario novoUsuario) {
+        usuarioRepository.save(novoUsuario);
+    }
+
+    public List<Usuario> buscarTodos(){
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario buscarPorId(Long id){
+        return usuarioRepository.getReferenceById(id);
+    }
+
+    public void atualizar(Usuario novoUsuario){
         usuarioRepository.save(novoUsuario);
     }
 }
