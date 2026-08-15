c1991c61baa5aa7548fc5079854eac40c9fcd644
Izaquiel Queiroz <kyelqueiroz@hotmail.com>
2026-08-15 14:53:02 -0300

    spring aula 3 e 4 -  Camadas e JPA

--- aula_3_camadas_jpa/sgp/src/main/java/com/treinarecife/sgp/controllers/UsuarioController.java
+++ aula_3_camadas_jpa/sgp/src/main/java/com/treinarecife/sgp/controllers/UsuarioController.java
@@ -4,6 +4,7 @@ import com.treinarecife.sgp.models.Usuario;
 import com.treinarecife.sgp.models.dto.UsuarioRequest;
 import com.treinarecife.sgp.models.dto.UsuarioResponse;
 import com.treinarecife.sgp.services.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -13,17 +14,13 @@ import java.util.List;
 @RequestMapping("usuarios")
 public class UsuarioController {
 
-    private final UsuarioService usuarioService;
-
-    private List<Usuario> usuarios = new ArrayList<>();
-
-    public UsuarioController(UsuarioService usuarioService) {
-        this.usuarioService = usuarioService;
-    }
+    @Autowired
+    private UsuarioService usuarioService;
 
     @GetMapping
     public List<UsuarioResponse> listar() {
         List<UsuarioResponse> resultado = new ArrayList<>();
+        List<Usuario> usuarios = usuarioService.buscarTodos();
         for (Usuario t : usuarios) {
             resultado.add(t.toDTO());
         }
@@ -31,10 +28,8 @@ public class UsuarioController {
     }
     @GetMapping("/{id}")
     public UsuarioResponse buscarPorId(@PathVariable Long id) {
-        for (Usuario t : usuarios) {
-            if (t.getId().equals(id)) return t.toDTO();
-        }
-        throw new RuntimeException("Tarefa não encontrada");
+        Usuario usuario = usuarioService.buscarPorId(id);
+        return usuario.toDTO();
     }
 
     @PostMapping
@@ -46,18 +41,19 @@ public class UsuarioController {
 
     @PutMapping("/{id}")
     public UsuarioResponse atualizar(@PathVariable Long id, @RequestBody UsuarioRequest req) {
-        for (Usuario u : usuarios) {
-            if (u.getId().equals(id)) {
-                u.setEmail(req.email());
-                return u.toDTO();
-            }
-        }
-        throw new RuntimeException("Tarefa não encontrada");
+        Usuario usuario = usuarioService.buscarPorId(id);
+        usuario.setNome(req.nome());
+        usuario.setCpf(req.cpf());
+        usuario.setEmail(req.email());
+        usuario.setSenha(req.senha());
+        usuario.setDataNascimento(req.dataNascimento());
+
+        usuarioService.atualizar(usuario);
+
+        return usuario.toDTO();
     }
     @DeleteMapping("/{id}")
     public void remover(@PathVariable Long id) {
-        for (int i = 0; i < usuarios.size(); i++) {
-            if (usuarios.get(i).getId().equals(id)) usuarios.remove(i);
-        }
+        usuarioService.deletar(id);
     }
 }
